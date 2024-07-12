bd93da9f2beb0d2c47883ceb34dd2ad0283fb1dc
Prakash Paneru <prkshpnr@gmail.com>
2024-07-12 11:12:15 +0545

    subtraction

--- 1.py
+++ 1.py
@@ -3,3 +3,10 @@ b=int(input("enter second number"))
 
 sum=a+b
 print("sum is:",sum)
+
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+
+diff=a-b
+print("difference is:",diff)
